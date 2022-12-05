931f996354670bcb26680fff79664025f0fc02d7
Beatriz de Brito <beatrizdb@ciandt.com>
2022-12-05 09:36:22 -0300

    Ajustes na navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path:'',
-    redirectTo:'listarPensamento',
-    pathMatch:'full'
-  },
-  {
-    path:'criarPensamento',
-    component: CriarPensamentoComponent
-  },
-  {
-    path:'listarPensamento',
-    component: ListarPensamentoComponent
-  },
-
+  { path: "", redirectTo: "listarPensamento", pathMatch: "full" },
+  { path: "criarPensamento", component: CriarPensamentoComponent },
+  { path: "listarPensamento", component: ListarPensamentoComponent },
 ];
 
 @NgModule({
